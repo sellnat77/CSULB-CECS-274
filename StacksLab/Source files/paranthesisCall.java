5187e296b0ebe15b9fc9ab8f481b6a7c65d69d1c
Russell Tan <sell_nat@yahoo.com>
2014-04-15 13:33:42 -0700

    Finished queue lab

--- StacksLab/Source files/paranthesisCall.java
+++ StacksLab/Source files/paranthesisCall.java
@@ -1,9 +1,14 @@
-
+/**
+ * Name: Russell Tan
+ * Class: CECS 274
+ * Description: This is the calling program for the parenthesis checker
+ * it asks for the user to enter an equation then checks if it is balanced
+ */
 public class paranthesisCall 
 {
 	public static void main(String args[])
 	{
-		parenthesisChecker check = new parenthesisChecker();
+		parenCheckStack check = new parenCheckStack();
 		
 		check.checkAnEquation();
 	}
