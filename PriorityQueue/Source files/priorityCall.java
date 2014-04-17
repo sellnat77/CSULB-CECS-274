64e5476c6531e083d23641be5effda15dbf11d72
Russell Tan <sell_nat@yahoo.com>
2014-04-17 12:39:15 -0700

    start or priority queue

---
+++ PriorityQueue/Source files/priorityCall.java
@@ -0,0 +1,13 @@
+
+public class priorityCall 
+{
+	public static void main(String args[])
+	{
+		priorityQueue office = new priorityQueue();
+		
+		office.assignJobs();
+		office.assignPriority();
+		office.carryOutPrint();
+	}
+
+}
